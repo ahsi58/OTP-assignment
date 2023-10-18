a453de0f3aa252b52591b7dfd0631eccd0c676fa
ahsi <patilisha2002@gmail.com>
2023-10-18 23:37:03 +0530

    otp version 2 modified

--- otp2.py
+++ otp2.py
@@ -24,20 +24,20 @@ def validate_mobile(num):
 # function to send otp via sms ans validate number
 def send_otp_over_mobile(target_no,fotp):
     
-    if(validate_mobile(target_no)):
+    # if(validate_mobile(target_no)):
         target_no="+91"+target_no
         message=client.messages.create(
           body="Your OTP is "+fotp,
           from_=keys.twilio_no,
           to=keys.target_no
         )
-        print(message.body)
+        # print(message.body)
       
-    else:
-       print("INVALID MOBILE NUMBER!")
-       target_no=input("ENTER AGAIN: ")
-       validate_mobile(target_no)
-       send_otp_over_mobile(target_no,fotp)
+    # else:
+    #    print("INVALID MOBILE NUMBER!")
+    #    target_no=input("ENTER AGAIN: ")
+    #    validate_mobile(target_no)
+    #    send_otp_over_mobile(target_no,fotp)
 
 #function to check email
 def validate_email(mail):
@@ -48,18 +48,18 @@ def validate_email(mail):
 
 # function to send otp via email and validate email
 def send_otp_over_email(mail,fotp,sender,password):
-   if(validate_email(mail)):
+#    if(validate_email(mail)):
         server=smtplib.SMTP('smtp.gmail.com',587)
         server.starttls()
         server.login(sender,password)
         msg_body="Your OTP is "+fotp
         server.sendmail(sender,mail,msg_body)
         print("OTP sent! ",fotp)
-   else:
-        print("INVALID MAIL!")
-        mail=input("ENTER AGAIN: ")
-        validate_email(mail)
-        send_otp_over_email(mail,fotp,sender,password)
+#    else:
+#         print("INVALID MAIL!")
+#         mail=input("ENTER AGAIN: ")
+#         validate_email(mail)
+#         send_otp_over_email(mail,fotp,sender,password)
       
  
 # drivers code
@@ -73,13 +73,25 @@ password="znnozwrslhfdavcu"
 if(ans.lower()=="mobile"):
     
     number=input("ENTER YOUR MOBILE NUMBER: ")
-    validate_mobile(number)
+    while(validate_mobile(number)==False):
+        number=input("invalid number")
+
     send_otp_over_mobile(number,fotp)
+    
+    # if(validate_mobile(number)):
+    #     send_otp_over_mobile(number,fotp)
+    # else:
+    #     print("Invalid number!")
+    #     number=input("Enter again!")
 
 elif(ans.lower()=="email"):
     
     recipent=input("ENTER YOUR MAIL ID: ")
-    send_otp_over_email(recipent,fotp,sender,password)
+    if(validate_email(recipent)):
+        send_otp_over_email(recipent,fotp,sender,password)
+    else:
+        print("Invalid email!")
+        recipent=input("Enter email again")
 
 # end of program
 
